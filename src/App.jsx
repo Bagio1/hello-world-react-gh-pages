235a0faab338b72b8e72077a7a7f1ad65af58c52
Bagio1 <mohamedfouhamy26@gmail.com>
2026-03-25 23:23:33 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="page">
+      <section className="card">
+        <p className="eyebrow">React GitHub Pages</p>
+        <h1>Bagio was here</h1>
+        <p className="description">
+          This project is ready did by Bagio
+        </p>
+      </section>
+    </main>
+  )
+}
+
+export default App
