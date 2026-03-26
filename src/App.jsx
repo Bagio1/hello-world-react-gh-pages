d3fbb01086ab6c87c75ce90bfa5dd65d96b752c9
Bagio1 <mohamedfouhamy26@gmail.com>
2026-03-26 10:49:26 +0400

    fix: tokennn

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <main className="page">
       <section className="card">
         <p className="eyebrow">React GitHub Pages</p>
-        <h1>Bagio     was here</h1>
+        <h1>Bagio was here</h1>
         <p className="description">
           This project is ready did by Bagio
         </p>
